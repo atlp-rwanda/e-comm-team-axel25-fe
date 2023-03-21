e7270dc10f2c02aeea19b92109d2bd02888a2110
muslim uwitondanishema <muslimuwitondanishema@gmail.com>
2023-03-21 14:03:29 +0200

    add sass pre-processing on the application.
    enable the use of both .scss and .css files on the client.
    Feature: #184749804

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import React from 'react';
-import './assets/styles/global.css';
+import './assets/styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

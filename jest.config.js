3e299915e9462d62f571d8f2cfbc692298994aae
SergeInGithub <nyiriserge@gmail.com>
2023-03-23 14:58:20 +0200

    ch(jest): setup jest tests [#184749807]

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+};
---
+++ src/components/Toggle.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const Toggle = () => {
+  const [on, setOn] = useState(false);
+  return <button onClick={() => setOn(!on)}>{on ? 'ON' : 'OFF'}</button>;
+};
+export default Toggle;

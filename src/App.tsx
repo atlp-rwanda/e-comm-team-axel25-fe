086d8c70ae35748021cd84168d54174a8ba55c1a
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-03-23 14:35:18 +0200

    add pre-commit lint hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import { AppRouter } from './lib/Router/AppRouter';
 import { routes } from './Routes';
 
-export const App = () => {  
+export function App() {
   return (
     <main>
       <AppRouter routes={routes} />
     </main>
   );
-};
+}
 export default App;

b0c3954689b4c3d8bc40f578c6e899b1c2b77854
muslim uwitondanishema <muslimuwitondanishema@gmail.com>
2023-04-11 21:50:25 +0200

    ft: add alert component #42
    this will be used to display alerts and warning
    
    feature: #42

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,18 +1,18 @@
-import { Alert } from './ui/Alert';
-import Footer from './shared/footer/Footer';
-import { Navbar } from './shared/navbar/Navbar';
-import { Container } from './Container';
-import { ProductCard } from './products/ProductCard';
-import { Counter } from './inputs/Counter';
-import { Sidebar } from './sidebar/Sidebar';
-import { Modal } from './shared/modals/Modal';
-import { Heading } from './Heading';
-import { InputField } from './inputs/InputField';
-import { SelectorField } from './inputs/SelectorField';
-import { Loader } from './Loader';
-import { RequireAuth } from './RequireAuth';
-import Dashboard from './Dashboard/Dashboard';
-import { HomeSlider } from './shared/HomeSlider';
+import { Alert } from "./ui/Alert";
+import Footer from "./shared/footer/Footer";
+import { Navbar } from "./shared/navbar/Navbar";
+import { Container } from "./Container";
+import { ProductCard } from "./products/ProductCard";
+import { Counter } from "./inputs/Counter";
+import { Sidebar } from "./sidebar/Sidebar";
+import { Modal } from "./shared/modals/Modal";
+import { Heading } from "./Heading";
+import { InputField } from "./inputs/InputField";
+import { SelectorField } from "./inputs/SelectorField";
+import { Loader } from "./Loader";
+import { RequireAuth } from "./RequireAuth";
+import Dashboard from "./Dashboard/Dashboard";
+import { HomeSlider } from "./shared/HomeSlider";
 
 export {
   Footer,
